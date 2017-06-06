b8409f45bdfd643f7b16f7b375613f568a73e97b
Chris <chriskim80@gmail.com>
2017-06-05 21:34:26 -0400

    Started working on StreamFilter

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -1,13 +1,16 @@
 package com.zipcodewilmington.streams;
 
 import com.zipcodewilmington.streams.anthropoid.Person;
-import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
+import static com.zipcodewilmington.streams.anthropoid.PersonFactory.*;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 
 /**
  * Created by leon on 5/2/17.
@@ -18,25 +21,25 @@ public class StreamFilter {
 
     /**
      * No arg constructor
-     */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
+     */
     public StreamFilter() {
-        this(Stream.empty(), null);
+        this(createPersonStream(100), (char)('A' + Math.random()*('Z'- 'A' + 1)));
     }
 
     /**
      * @param people - Array of person objects
      * @param startingCharacter - character to filter by
-     */ //TODO
+     */
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(Arrays.stream(people), startingCharacter);
     }
 
     /**
      * @param people - List of person objects
      * @param startingCharacter - character to filter by
-     */ //TODO
+     */
     public StreamFilter(List<Person> people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this(people.stream(), startingCharacter);
     }
 
 
@@ -55,7 +58,8 @@ public class StreamFilter {
      * @return a list of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListMultiLine() {
-        return null;
+        return personStream.filter(p -> p.getName().startsWith(startingCharacter.toString()))
+                .collect(Collectors.toList());
     }
 
 
